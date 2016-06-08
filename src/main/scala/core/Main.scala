7b0f46a075db7f7b65cd004625bb4e6b8816f056
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-06-08 12:19:26 +0300

    fix output

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -10,6 +10,7 @@ import geotrellis.raster.io.geotiff._
 
 import scala.concurrent.{ExecutionContext, Future}
 import java.util.concurrent.Executors
+import java.io.File
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -17,6 +18,7 @@ object Main {
       case Some(config) => {
         val executor = Executors.newFixedThreadPool(config.threads)
         implicit val executionContext = ExecutionContext.fromExecutor(executor)
+        val regexp = """^LC8(\d{6})(\d{4})(\d{3})(.)*""".r
 
         Future.sequence(Landsat8Query()
           .withStartDate(config.getStartDate)
@@ -25,15 +27,18 @@ object Main {
           .intersects(GeoJson.fromFile[Polygon](config.polygon))
           .collect()
           .map { img => Future {
+            val output = regexp.findAllIn(img.sceneId).matchData.map(s => s"${config.output}/${s.group(2)}/${s.group(3)}").next
+            new File(output).mkdirs
+
             val lr =
               if(img.imageExistsS3()) img.getFromS3(config.bands)
               else img.getFromGoogle(config.bands)
             val raster = lr.raster
             if(config.multiband)
-              GeoTiff(raster.raster, raster.crs).write(s"${config.output}/${img.sceneId}_B_${config.bands.mkString("")}.tif")
+              GeoTiff(raster.raster, raster.crs).write(s"${output}/${img.sceneId}_B_${config.bands.mkString("")}.tif")
             else
               raster.raster.bands.zip(config.bands).foreach { case (tile, i) =>
-                GeoTiff(Raster(tile, raster.extent), raster.crs).write(s"${config.output}/${img.sceneId}_B_${i}.tif")
+                GeoTiff(Raster(tile, raster.extent), raster.crs).write(s"${output}/${img.sceneId}_B_${i}.tif")
               }
           } }) onComplete {
           case _ => executor.shutdown()
